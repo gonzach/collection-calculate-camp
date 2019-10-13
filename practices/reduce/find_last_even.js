a1a9ab6ff13ea96de1f4462ccd216868e5c4fd8e
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-13 20:36:40 +0800

    Find last even

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -3,8 +3,7 @@
 function find_last_even(collection) {
   
   return collection.filter(collectionObject => collectionObject % 2 == 0).pop();
- 
-          
+       
 }
 
 module.exports = find_last_even;
