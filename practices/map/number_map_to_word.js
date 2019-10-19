72628d7914389bc028879dd9172efea43d2638c4
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-19 11:31:49 +0800

    - fix number map to word

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,9 @@
 'use strict';
+
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+
+  return collection.map(numberList => String.fromCharCode(number+64).toLowerCase());
+
 };
 
 module.exports = number_map_to_word;
