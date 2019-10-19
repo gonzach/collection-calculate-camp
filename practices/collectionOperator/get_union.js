756e398435223e572a24f7f4b388e1b1d4473e29
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-19 11:10:09 +0800

    - Fix get union

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,15 +1,7 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-  //List<Integer> unionA = collection_a.stream.collect(Collectors.toList();
- // List<Integer> unionB = collection_b.stream.collect(Collectors.toList();
- const res = [];
- return res = Arrays.stream(collection_a);
- 
-//return Arrays.stream(collection_a).collect(Collectors.toList());
-  //return collection_a.stream.collect(Collectors.toList()).addAll(collection_b.stream.collect(Collectors.toList()));
-  //List<Integer> distinctUnion = unionA.stream().distinct().collect(Collectors.toList());
-  //return distinctUnion;
+  return Array.from(new Set([...collection_a, ...collection_b]));
 }
 
 module.exports = get_union;
