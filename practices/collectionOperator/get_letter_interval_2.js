24c2f2039268cb5ab048665a39100afb70665fbc
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 16:00:23 +0800

    - add union

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //write code here
+  let result = [];
+  let next, first;
+  for (let i = number_a; i <= number_b; i++) {
+    first = (i - 1) % 26;
+    next = String.fromCharCode(65 + first) + next;
+    result.push(next);
+    i = (i - first)/26 | 0;
+  }
+  return next;
 }
 
 module.exports = get_letter_interval_2;
