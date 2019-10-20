8af843fb2f4877bf31881806b87adf107297aa83
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 16:01:17 +0800

    - fix compute median

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compute_median(collection) {
-  //write code here
+
+    let collections = collection.slice(0).sort((a,b) => a - b);
+    let half = Math.floor(collections.length/2);
+
+    if(collections.length % 2)
+        return collections[half];
+    else
+        return (collections[half-1] + collections[half]) / 2.0;
 }
 
 module.exports = compute_median;
