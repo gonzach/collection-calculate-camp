2088e6db29ce79d4da80670cbc8cfddbf8f22da7
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 16:01:05 +0800

    - add chain message

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //write code here
+
+    let splitCollection = collection.split('->');
+    let collections = splitCollection.map(number => parseInt(number,10)).sort((a,b) => a-b);
+    let half = Math.floor(collections.length/2);
+
+    if(collections.length % 2)
+        return collections[half];
+    else
+        return (collections[half-1] + collections[half]) / 2.0;
+
 }
 
 module.exports = compute_chain_median;
