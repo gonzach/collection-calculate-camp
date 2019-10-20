a7de93c4b4368f3c8440e31c0f76bff148dbec8f
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-20 16:00:47 +0800

    - add chain message

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  
+
   return (JSON.stringify(collection_a)==JSON.stringify(collection_b));
+
 }
 
 module.exports = compare_collections;
