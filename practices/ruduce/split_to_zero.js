597798c5f8a7a795ebdbff355e118e1797416e66
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-21 08:51:32 +0800

    ADDED

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,19 +1,14 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
+
   let splitResult = [];
-  let num = 0;
-  for( num = number; num <= 0; ){
-    console.log(num);
+  splitResult.push(number);
+  for(let num = number; num > 0; ){
     num = num - interval;
+      splitResult.push((num).toFixed(1));
     }
-
-     let length = Math.trunc(number/interval) + 1;
-      if(number % interval !== 0)
-        length++;
-
-      number += interval;
-    return Array.from(Array(length),(x,y) => number= parseFloat((number - interval).toFixed(1)));
+    return splitResult;
 }
 
 module.exports = spilt_to_zero;
